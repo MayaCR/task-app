c42ff791676cb6cb4968348d1230b86fc72434ca
Kymia R. <106397404+MayaCR@users.noreply.github.com>
2024-11-12 12:54:28 -0500

    Add menu button event listeners

--- src/main.js
+++ src/main.js
@@ -1,3 +1,12 @@
 import './styles/main.css'
+import { updateTaskView } from './modules/UIManager'
 
-console.log('Connected')
+const inboxBtn = document.querySelector('#inboxBtn')
+const todayBtn = document.querySelector('#todayBtn')
+const upcomingBtn = document.querySelector('#upcomingBtn')
+const allBtn = document.querySelector('#allBtn')
+
+inboxBtn.addEventListener('click', updateTaskView)
+todayBtn.addEventListener('click', updateTaskView)
+upcomingBtn.addEventListener('click', updateTaskView)
+allBtn.addEventListener('click', updateTaskView)
